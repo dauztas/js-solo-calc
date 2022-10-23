56892f09f134b57b5f25b93dafd3fa42ea09bd90
dauztas <ovid.pp@yahoo.com>
2022-10-23 18:59:54 +0300

    buttons and output func

---
+++ calc-app.js
@@ -0,0 +1,76 @@
+window.addEventListener("DOMContentLoaded", init);
+const options = [
+  "/",
+  "*",
+  "+",
+  "-",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "0",
+  "."
+];
+
+const specials = ["/", "*", "+", "-"];
+
+function init() {
+  document.title = "Javascript Calculator";
+  console.log("ready");
+  const container = document.createElement("div");
+  container.classList.add("container");
+  container.style.maxWidth = "600px";
+  container.style.margin = "auto";
+  document.body.appendChild(container);
+  const output = document.createElement("input");
+  output.setAttribute("type", "text");
+  output.classList.add("output");
+  output.style.width = "100%";
+  output.style.lineHeight = "50px";
+  output.style.fontSize = "3em";
+  output.style.textAlign = "right";
+  container.appendChild(output);
+  const main = document.createElement("div");
+  main.classList.add("main");
+  main.style.width = "100%";
+  container.appendChild(main);
+  options.forEach(function (val) {
+    //console.log(val);
+    buttonMaker(val, addOutput);
+  });
+
+  buttonMaker("=", evalOutput);
+  buttonMaker("c", clrOutput);
+
+  function evalOutput() {
+    console.log("=");
+    if (output.value === "") {
+      output.style.border = "red 1px solid";
+    }
+  }
+
+  function buttonMaker(txt, myFunction) {
+    let button = document.createElement("button");
+    button.setAttribute("type", "button");
+    button.style.width = "23%";
+    button.style.lineHeight = "50px";
+    button.style.margin = "1%";
+    button.style.fontSize = "2em";
+    button.val = txt;
+    button.textContent = txt;
+    button.addEventListener("click", myFunction);
+    main.appendChild(button);
+  }
+
+  function addOutput(e) {
+    console.log(e.target.val);
+    output.style.border = "black 1px solid";
+    let character = e.target.val;
+    output.value += character;
+  }
+}
