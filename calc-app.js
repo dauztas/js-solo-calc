9048ef9378f754d7492254f20749dda561b174cf
dauztas <ovid.pp@yahoo.com>
2022-10-23 20:40:40 +0300

    Final commit

--- calc-app.js
+++ calc-app.js
@@ -1,5 +1,9 @@
-window.addEventListener("DOMContentLoaded", init);
-const options = [
+//Solely Javascript calculator
+
+window.addEventListener("DOMContentLoaded", init); // Eventlistener to fire off whenever the html document has loaded and parsed.
+
+// Keys array.
+const optionalKeys = [
   "/",
   "*",
   "+",
@@ -17,37 +21,41 @@ const options = [
   "."
 ];
 
-const specials = ["/", "*", "+", "-"];
+// Special keys array.
+const specialKeys = ["/", "*", "+", "-"];
 
+// Main function
 function init() {
+  console.log("Ready");
   document.title = "Javascript Calculator";
-  console.log("ready");
+  alert("WELCOME \nJavascript Calculator is ready to be used"); // Greeting alert.
   let decimal = false;
   let evaluation = false;
-  const container = document.createElement("div");
+  const container = document.createElement("div"); //
   container.classList.add("container");
   container.style.maxWidth = "600px";
   container.style.margin = "auto";
-  document.body.appendChild(container);
-  const output = document.createElement("input");
+  document.body.appendChild(container); // Creating, styling and appending container div to the body element.
+  const output = document.createElement("input"); //
   output.setAttribute("type", "text");
   output.classList.add("output");
   output.style.width = "100%";
   output.style.lineHeight = "50px";
   output.style.fontSize = "3em";
   output.style.textAlign = "right";
-  container.appendChild(output);
-  const main = document.createElement("div");
+  container.appendChild(output); // Creating an input element, giving it styling, giving it a class and appending to the previously created container element.
+  const main = document.createElement("div"); //
   main.classList.add("main");
   main.style.width = "100%";
-  container.appendChild(main);
-  options.forEach(function (val) {
+  container.appendChild(main); // Creating, styling, adding a class and appending main div to the previously created container element.
+  optionalKeys.forEach(function (val) {
     buttonMaker(val, addOutput);
   });
 
   buttonMaker("=", evalOutput);
   buttonMaker("c", clrOutput);
 
+  // Function to color the output border on error or not.
   function colorOutput(v) {
     output.style.border = v + " 3px solid";
   }
@@ -59,15 +67,20 @@ function init() {
       colorOutput("red");
     } else if (evaluation) {
       colorOutput("red");
+      alert(
+        "Input has to end with a number before pressing the equality button"
+      );
     } else {
       output.value = evaluation(output.value);
     }
   }
 
+  // Function to clear the output.
   function clrOutput() {
     output.value = "";
   }
 
+  // Button making function
   function buttonMaker(txt, myFunction) {
     let button = document.createElement("button");
     button.setAttribute("type", "button");
@@ -81,8 +94,9 @@ function init() {
     main.appendChild(button);
   }
 
+  // Func to make sure decimals doesnt have decimals in them.
+
   function addOutput(e) {
-    console.log(decimal);
     colorOutput("black");
     let character = e.target.val;
 
@@ -94,7 +108,7 @@ function init() {
         decimal = true;
       }
     }
-    evaluation = specials.includes(character);
+    evaluation = specialKeys.includes(character);
     if (evaluation) {
       decimals = false;
     }
